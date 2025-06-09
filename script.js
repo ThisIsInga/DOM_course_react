28cdc6145048c99d760c3aae422cc1d2e87ae37f
ThisIsInga <155862681+ThisIsInga@users.noreply.github.com>
2025-06-09 18:41:20 +1000

    Исправила работу кода

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const elementRoot = document.getElementById('root');
 const appContainer = document.createElement('div');
-appContainer.textContent = 'Моё первое приложение';
 appContainer.innerHTML = `
+    Моё первое приложение
     <p>Это первый параграф.</p>
     <button>Нажми на меня!</button>
 `;
