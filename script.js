9b7e011fbd1419238f5b834b334659691546198b
ThisIsInga <155862681+ThisIsInga@users.noreply.github.com>
2025-06-10 18:08:38 +1000

    React block 3

--- script.js
+++
@@ -1,8 +0,0 @@
-const elementRoot = document.getElementById('root');
-const appContainer = document.createElement('div');
-appContainer.innerHTML = `
-    Моё первое приложение
-    <p>Это первый параграф.</p>
-    <button>Нажми на меня!</button>
-`;
-elementRoot.appendChild(appContainer);
